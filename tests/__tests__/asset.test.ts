9eb24a78d0e4e00b32c5b94d2ddc19f04d27c274
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-19 15:46:21 +0530

    successfully create asset object

--- tests/__tests__/asset.test.ts
+++ tests/__tests__/asset.test.ts
@@ -1,38 +1,50 @@
+import { ApiClient } from "../../src/api-client";
 import { Asset } from "../../src/asset";
 import {
   InvalidArgumentError,
   MissingArgumentError,
 } from "../../src/errors/app.errors";
 import { setupFetchMock } from "../fetch.setup";
+import config from "../__mocks__/roundware.config";
 
 describe("Asset ", () => {
   setupFetchMock();
-  it("should throw error is params not passed", () => {
-    expect.assertions(1);
-    try {
-      // @ts-expect-error
-      const asset = new Asset();
-    } catch (e) {
-      expect(e).toBeInstanceOf(MissingArgumentError);
-    }
-  });
-  it("should throw invalid if projectId is not number", () => {
-    expect.assertions(1);
-    try {
-      // @ts-expect-error
-      const asset = new Asset("10");
-    } catch (e) {
-      expect(e).toBeInstanceOf(InvalidArgumentError);
-    }
-  });
 
-  it("should throw invalid if apiClient is not instance of apiClient", () => {
-    expect.assertions(1);
-    try {
-      // @ts-expect-error
-      const asset = new Asset(10, { apiClient: new Date() });
-    } catch (e) {
-      expect(e).toBeInstanceOf(InvalidArgumentError);
-    }
+  describe("Instantiation", () => {
+    it("should throw error is params not passed", () => {
+      expect.assertions(1);
+      try {
+        // @ts-expect-error
+        const asset = new Asset();
+      } catch (e) {
+        expect(e).toBeInstanceOf(MissingArgumentError);
+      }
+    });
+    it("should throw invalid if projectId is not number", () => {
+      expect.assertions(1);
+      try {
+        // @ts-expect-error
+        const asset = new Asset("10");
+      } catch (e) {
+        expect(e).toBeInstanceOf(InvalidArgumentError);
+      }
+    });
+
+    it("should throw invalid if apiClient is not instance of apiClient", () => {
+      expect.assertions(1);
+      try {
+        // @ts-expect-error
+        const asset = new Asset(10, { apiClient: new Date() });
+      } catch (e) {
+        expect(e).toBeInstanceOf(InvalidArgumentError);
+      }
+    });
+
+    it("should create a Asset instance successfully", () => {
+      const asset = new Asset(config.projectId, {
+        apiClient: new ApiClient(config.baseServerUrl),
+      });
+      expect(asset).toBeInstanceOf(Asset);
+    });
   });
 });
