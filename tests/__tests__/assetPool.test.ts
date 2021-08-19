fddb05b80292aaffd9d78aa5a54106f91fac9153
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-19 19:55:36 +0530

    test against raw data

--- tests/__tests__/assetPool.test.ts
+++ tests/__tests__/assetPool.test.ts
@@ -2,6 +2,7 @@ import { assetDecorationMapper, AssetPool } from "../../src/assetPool";
 import { AssetSorter } from "../../src/assetSorter";
 import { InvalidArgumentError } from "../../src/errors/app.errors";
 import { IAssetData, IMixParams } from "../../src/types";
+import { coordsToPoints } from "../../src/utils";
 import { setupFetchMock } from "../fetch.setup";
 import {
   MOCK_ASSET_DATA,
@@ -94,9 +95,111 @@ describe("AssetPool", () => {
       assetPool.assets = undefined;
 
       assetPool.updateAssets(MOCK_ASSET_DATA.reverse(), MOCK_TIMED_ASSET_DATA);
-      expect(assetPool.assets).toEqual(
-        MOCK_ASSET_DATA.map(assetDecorationMapper(MOCK_TIMED_ASSET_DATA))
-      );
+      console.log(assetPool.assets);
+      expect(assetPool.assets).toEqual([
+        {
+          locationPoint: coordsToPoints({
+            latitude: 1,
+            longitude: 1,
+          }),
+          playCount: 0,
+          activeRegionLength: 7.407,
+          activeRegionUpperBound: 7.407,
+          activeRegionLowerBound: 0,
+          id: 5936,
+          description: "",
+          latitude: 1,
+          longitude: 1,
+          shape: null,
+          filename: "20150119-113758-18467.wav",
+          file: "//prod.roundware.com/rwmedia/20150119-113758-18467.mp3",
+          volume: 1,
+          submitted: true,
+          created: new Date("2015-01-19T06:07:59.000Z"),
+          updated: "2015-01-19T11:37:59",
+          weight: 50,
+          start_time: 0,
+          end_time: 7.407,
+          user: null,
+          media_type: "audio",
+          audio_length_in_seconds: 7.41,
+          tag_ids: [92],
+          session_id: 18467,
+          project_id: 10,
+          language_id: 1,
+          envelope_ids: [3392],
+          description_loc_ids: [],
+          alt_text_loc_ids: [],
+        },
+        {
+          locationPoint: coordsToPoints({
+            latitude: 42.4987144470215,
+            longitude: -71.2809524536133,
+          }),
+          playCount: 0,
+          activeRegionLength: 3.436,
+          activeRegionUpperBound: 3.436,
+          activeRegionLowerBound: 0,
+          id: 5935,
+          description: "",
+          latitude: 42.4987144470215,
+          longitude: -71.2809524536133,
+          shape: null,
+          filename: "20150117-231843-18447.wav",
+          file: "//prod.roundware.com/rwmedia/20150117-231843-18447.mp3",
+          volume: 1,
+          submitted: true,
+          created: new Date("2015-01-17T17:48:44.000Z"),
+          updated: "2015-01-17T23:18:44",
+          weight: 50,
+          start_time: 0,
+          end_time: 3.436,
+          user: null,
+          media_type: "audio",
+          audio_length_in_seconds: 3.44,
+          tag_ids: [92],
+          session_id: 18447,
+          project_id: 10,
+          language_id: 1,
+          envelope_ids: [3390],
+          description_loc_ids: [],
+          alt_text_loc_ids: [],
+        },
+        {
+          locationPoint: coordsToPoints({
+            latitude: 42.4986343383789,
+            longitude: -71.2810440063477,
+          }),
+          playCount: 0,
+          activeRegionLength: 11.284,
+          activeRegionUpperBound: 11.284,
+          activeRegionLowerBound: 0,
+          id: 5934,
+          description: "",
+          latitude: 42.4986343383789,
+          longitude: -71.2810440063477,
+          shape: null,
+          filename: "20150117-231643-18447.wav",
+          file: "//prod.roundware.com/rwmedia/20150117-231643-18447.mp3",
+          volume: 1,
+          submitted: true,
+          created: new Date("2015-01-17T17:46:44.000Z"),
+          updated: "2015-01-17T23:16:44",
+          weight: 50,
+          start_time: 0,
+          end_time: 11.284,
+          user: null,
+          media_type: "audio",
+          audio_length_in_seconds: 11.28,
+          tag_ids: [91],
+          session_id: 18447,
+          project_id: 10,
+          language_id: 1,
+          envelope_ids: [3389],
+          description_loc_ids: [],
+          alt_text_loc_ids: [],
+        },
+      ]);
     });
   });
 });
