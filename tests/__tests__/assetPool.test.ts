bb75b70885fe90c2503e2f82bd45cf6eaee81c14
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2021-08-19 19:22:02 +0530

    should update the assets by decorating with timedAssets and sorting

--- tests/__tests__/assetPool.test.ts
+++ tests/__tests__/assetPool.test.ts
@@ -36,15 +36,37 @@ describe("AssetPool", () => {
       expect(assetPool).toBeInstanceOf(AssetPool);
     });
 
-    it("should update the assets by decorating with timedAssets", () => {
+    it("should update the assets by decorating with timedAssets and sorting", () => {
       const assetPool = new AssetPool({
         assets: MOCK_ASSET_DATA,
         timedAssets: MOCK_TIMED_ASSET_DATA,
+        sortMethods: [],
+        mixParams: {
+          ordering: "by_like",
+        },
       });
 
-      expect(assetPool.assets).toEqual(
-        MOCK_ASSET_DATA.map(assetDecorationMapper(MOCK_TIMED_ASSET_DATA))
+      const expectedData = MOCK_ASSET_DATA.map(
+        assetDecorationMapper(MOCK_TIMED_ASSET_DATA)
       );
+      assetPool.assetSorter.sort(expectedData);
+      expect(assetPool.assets).toEqual(expectedData);
+    });
+
+    it("should set assetSorter with AssetSorter object", () => {
+      const assetPool = new AssetPool({
+        assets: MOCK_ASSET_DATA,
+        timedAssets: MOCK_TIMED_ASSET_DATA,
+
+        mixParams: {
+          ordering: "by_like",
+        },
+      });
+      const expectedAssetSorter = new AssetSorter({
+        sortMethods: [],
+        ordering: "by_like",
+      });
+      expect(assetPool.assetSorter).toEqual(expectedAssetSorter);
     });
   });
 });
